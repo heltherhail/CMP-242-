9503546d1307206b3819c3f4781f26d3780a3b89
heltherhail <helther3438@gmail.com>
2026-02-24 10:52:26 +0200

    just updated the student code

--- projects/oop-attendance-system/student.py
+++ projects/oop-attendance-system/student.py
@@ -2,7 +2,25 @@ class Student:
     def __init__(self, name,student_id):
         self.name = name
         self.student_id = student_id 
+        self.__attendance_count = 0
+        self.__total_classes = 0
+
+    def mark_attendance(self, status):
+        self.__total_classes += 1
+        if status == 'present':
+            self.__attendance_count += 1 
+
+    def add_class_day(self):
+        self.__total_classes += 1
+
+    def calculate_attendance_percentage(self):
+        if self.__total_classes == 0:
+            return 0
+        return (self.__attendance_count / self.__total_classes) * 100
+
+    def is_allowed_exam(self):
+        return (self.calculate_attendance_percentage() >=70)          
 
     def display_info(self):
-        return f"{self.student_id} - {self.name}"
+        return f"{self.student_id} - {self.name}: {self.calculate_attendance_percentage():.2f}% attendance" and "Allowed to take exam" if self.is_allowed_exam() else "Not allowed to take exam"
        
