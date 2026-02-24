33bbd49052e0f77583a3c82bf8129a9f84b49464
heltherhail <helther3438@gmail.com>
2026-02-24 11:41:10 +0200

    updated main file

--- projects/oop-attendance-system/main.py
+++ projects/oop-attendance-system/main.py
@@ -1,16 +1,40 @@
-from student import Student
-from attendance import Attendance
+from attendance_system import AttendanceSystem
+from qr_manager import QRManager
 
-#Create attendance manager
-attendance = Attendance()
+system = AttendanceSystem()
+qr = QRManager()
 
-#Create students
-student1 = Student("Nancy", "001")
-student2 = Student("Gide", "002")
+while True:
+    print("\n1. Add Student")
+    print("2. Generate QR Code for Attendance")
+    print("3. Mark Attendance")
+    print("4. Add Class Day")
+    print("5. Generate Report")
+    print("6. Exit")
 
-#Mark attendance
-attendance.mark_attendance(student1, "Present")
-attendance.mark_attendance(student2, "Absent")
+    choice = input("Enter your choice: ")
 
-#Show records
-attendance.show_attendance()
+    if choice == '1':
+        student_id = input("Enter student ID: ")
+        name = input("Enter student name: ")
+        system.add_student(student_id, name)
+        
+    elif choice == '2':
+        student_id = input("Enter student ID: ")
+        qr.generate_qr_code(student_id)
+        
+    elif choice == '3':
+        sid = input("Scan QR code or enter student ID: ")
+        system.mark_attendance(sid)
+        
+    elif choice == '4':
+        system.add_class_day()
+        print("New class day added successfully.")
+
+    elif choice == '5':
+        system.generate_report()
+        
+    elif choice == '6':
+        break
+    else:
+        print("Invalid choice. Please try again.") 
