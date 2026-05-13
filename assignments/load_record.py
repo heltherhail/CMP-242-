a3238d43f1472a5b7369cb749d14e937e78d2393
heltherhail <helther3438@gmail.com>
2026-05-13 18:32:15 +0300

    renewed load record code

--- assignments/load_record.py
+++ assignments/load_record.py
@@ -1,16 +1,22 @@
 import pickle
 
 def load_records(filename):
+
     try:
         with open(filename, "rb") as file:
-            records = pickle.load(file)
 
-        print("\nStored Records:")
-        for record in records:
-            print(f"Name: {record['name']}, Student ID: {record['student_id']}")
+            while True:
+
+                try:
+                    record = pickle.load(file)
+
+                    print(record)
+
+                except EOFError:
+                    break
 
     except FileNotFoundError:
-        print("File not found.")
-    
-    except Exception as e:
-        print("Error:", e)
+        print("File not found")
+
+
+load_records("students.dat")
