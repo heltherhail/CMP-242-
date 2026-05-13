b5c09e908d8a5b7303e21b22dddd9198a1d9f584
heltherhail <helther3438@gmail.com>
2026-05-13 18:23:21 +0300

    edited my dump record code

--- assignments/dump_record.py
+++ assignments/dump_record.py
@@ -1,15 +1,27 @@
 import pickle
 
 def dump_records(filename):
-    records = []
-    
-    n = int(input("Enter number of records: "))
-    
-    for i in range(n):
-        print(f"\nRecord {i+1}")
-        name = input("Enter name: ")
-        student_id = int(input("Enter student ID: "))
-        
-        record = {"name": name, "student_id": student_id}
-        records.append(record)
 
+    with open(filename, "wb") as file:
+
+        n = int(input("How many records? "))
+
+        for i in range(n):
+
+            print(f"\nRecord {i+1}")
+
+            name = input("Enter name: ")
+            student_id = int(input("Enter student ID: "))
+
+            record = {
+                "name": name,
+                "student_id": student_id}
+
+            records = []
+            records.append(record)
+
+            pickle.dump(record, file)
+
+    print("Records saved successfully!")
+
+dump_records("students.dat")
